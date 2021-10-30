de26bb893726043d8685b6b9c28fa18ae2345336
Wei <xavier840923@gmail.com>
2021-10-30 10:42:46 +0800

    任務03、04:使用內嵌繫結&屬性繫結來顯示資料\

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-Implementation';
+  title = 'conduitxxxx';
+  subtitle = 'A place to share your <u>knowledge</u>.';
 }
